455dbe05f342c29835c5251c1cfdbd4f3b67abac
Hilal06 <rifaulhilal06@gmail.com>
2019-12-17 10:02:03 +0700

    Update index

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const {Builder, By, Key, util} = require('selenium-webdriver');
 
 async function example() {
     // bot quisioner
+    const NIM = '';
+    // Ubah kode berikut dengan xpath quisioner yang dikerjakan
+    // ex : '/html/body/div[3]/div/div[1]/table/tbody/tr[6]/td[5]/a'
+    let link = '';
+
+
     let driver = await new Builder().forBrowser('firefox').build();
     await driver.get('http://ujian.jti.polinema.ac.id/');
-    await driver.findElement(By.id('xuser_name')).sendKeys('1741720195');
-    await driver.findElement(By.id('xuser_password')).sendKeys('1741720195');
+    await driver.findElement(By.id('xuser_name')).sendKeys(NIM);
+    await driver.findElement(By.id('xuser_password')).sendKeys(NIM);
     await driver.findElement(By.id('login')).click()
     await driver.sleep(3000);
-
-    // Ubah kode berikut dengan xpath quisioner yang dikerjakan
-    let link = '/html/body/div[3]/div/div[1]/table/tbody/tr[6]/td[5]/a';
 
     let kerjakan = '/html/body/div[3]/div/div[3]/a[1]';
     await driver.findElement(By.xpath(link)).click();
@@ -24,6 +27,8 @@ async function example() {
         let ans = By.id('answertext');
         let next = By.id('nextquestion');
         let terminate = By.id('terminatetest');
+
+        // Nilai Quisioner 1 - 5 Random;
         num = ((num + index)%2) + 4;
         await driver.findElement(ans).sendKeys(num);
         await driver.findElement(next).click();
